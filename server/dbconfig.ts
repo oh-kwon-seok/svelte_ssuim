0974047303820fbc06c9bb20c1da3ef5150d9c3e
oh-kwon-seok <ohks486@naver.com>
2023-05-18 11:54:48 +0900

    a

--- server/dbconfig.ts
+++ server/dbconfig.ts
@@ -3,8 +3,8 @@ const dbconfig : any = {
     "host": "localhost",
     "port": 3306,
     "username": "root",
-    "password": "1234",
-    "database": "nest_test",
+    "password": "1111",
+    "database": "nest_dev",
     "entities": ["dist/src/**/**.entity{.ts,.js}"],
     "synchronize": true,
   }
